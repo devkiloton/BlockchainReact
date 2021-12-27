430765a2be2fe87cba2c5ba8bb2bf15a2e2f9788
devkiloton <dev.kiloton@gmail.com>
2021-12-27 13:41:48 -0300

    disabled start/stop and creating TaskGenerator component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="box">
     <div class="columns">
-      <div class="column is-8" aria-label="Form to create a new task">
+      <div class="column is-8" role="form" aria-label="Form to create a new task">
         <input type="text" class="input" placeholder="Which task do you want to do?"/>
       </div>
-      <div class="column is-flex is-align-items-center is-justify-content-space-between">
-        <section>
-          <strong>{{time}}</strong>
-        </section>
-        <button class="button" @click="start">
-          <span class="icon">
-            <i class="fas fa-play"></i>
-          </span>
-          <span>Play</span>
-        </button>
-        <button class="button" @click="stop">
-          <span class="icon">
-            <i class="fas fa-stop"></i>
-          </span>
-          <span>Stop</span>
-        </button>
+      <div class="column">
+        <TaskGenerator/>
       </div>
     </div>      
   </div>
@@ -27,27 +13,10 @@
 
 <script lang='ts'>
 import { defineComponent } from "vue";
+import TaskGenerator from "./TaskGenerator.vue";
 export default defineComponent({
-  name: 'Form',
-  data() {
-    return {
-      timeInSeconds: 0,
-      timerId: 0
-    }
-  },
-  computed:{
-    time(): string{
-      return new Date(this.timeInSeconds * 1000).toISOString().substr(11,8);
-    }
-  },
-  methods: {
-    start(){
-      this.timerId = setInterval(() => this.timeInSeconds++, 1000)
-    },
-    stop(){
-      clearInterval(this.timerId);
-    }
-  }
+  components: { TaskGenerator },
+  name: 'Form'
 })
 </script>
 
---
+++ src/components/TaskGenerator.vue
@@ -0,0 +1,47 @@
+<template>
+      <div class="column is-flex is-align-items-center is-justify-content-space-between">
+        <Timer :timeInSeconds="timeInSeconds"/>
+        <button class="button" @click="start" :disabled="timerRunning">
+          <span class="icon">
+            <i class="fas fa-play"></i>
+          </span>
+          <span>Play</span>
+        </button>
+        <button class="button" @click="stop" :disabled="!timerRunning">
+          <span class="icon">
+            <i class="fas fa-stop"></i>
+          </span>
+          <span>Stop</span>
+        </button>
+      </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Timer from "./Timer.vue";
+export default defineComponent({
+  components: { Timer },
+  name: 'TaskGenerator',
+  data() {
+    return {
+      timeInSeconds: 0,
+      timerId: 0,
+      timerRunning: false
+    }
+  },
+  methods: {
+    start(){
+      this.timerRunning = true;
+      this.timerId = setInterval(() => this.timeInSeconds++, 1000)
+    },
+    stop(){
+      this.timerRunning = false;
+      clearInterval(this.timerId);
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
